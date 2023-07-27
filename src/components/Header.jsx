da423cbcb788e45e185f5e994d463e0211e80298
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-07-26 22:28:50 -0400

    update code

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <div class="header">
+        <div class="logo"><img src="./assets/pursuit-logo.png" alt="Pursuit logo" /></div>
+        <div class="menu">
+        <ul>
+            <li>
+            <a href="https://pursuit.org">Pursuit Home Page</a>
+            </li>
+            <li>
+            <a href="https://github.com/joinpursuit">Pursuit GitHub</a>
+            </li>
+        </ul>
+        </div>
+  </div>
+    )
+}
+export default Header;
---
+++ src/components/Primary.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Primary = () => {
+    return (
+        <div class="primary-content">
+            <div class="sidebar">
+                <p>Sidebar.</p>
+            </div>
+
+            <div class="main">
+                <p>Main content goes here.</p>
+            </div>
+        </div>
+    )
+}
+export default Primary;
